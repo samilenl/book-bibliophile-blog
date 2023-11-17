dfbd17bf299a91e4db00efbe0db85fd33b4871a6
Samuel Ilenloa <sam.ilenloa@gmail.com>
2023-11-17 19:10:03 +0100

    Scroll to Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Outlet } from "react-router-dom"
+import ScrollToTop from "./components/ScrollToTop"
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <div className="container">
           <div className="content">
